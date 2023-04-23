4143c91c7b9668a3dc77440796e55ea0bec0917f
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-04-23 15:33:38 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tungtee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tungtee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Pages/welcome.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            "Tung Tee",
+            style: TextStyle(fontSize: 47),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          MyButton()
+        ],
+      )),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Text('Get Start'),
+          SizedBox(
+            width: 5,
+          ),
+          Icon(
+            // <-- Icon
+            Icons.arrow_forward_ios_rounded,
+            size: 15.0,
+          ), // <-- Text
+        ],
+      ),
+    );
+  }
+}
