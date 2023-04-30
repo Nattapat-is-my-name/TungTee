19bec1baf8dee494e4b5a6affad8a4a58c53b1eb
MrT-jesus-1995 <livemlg1995@gmail.com>
2023-04-30 16:56:27 +0700

    basic info

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tungtee/Pages/welcome.dart';
+import 'package:tungtee/Pages/BasicInformation.dart';
 
 void main() {
   runApp(const MyApp());
