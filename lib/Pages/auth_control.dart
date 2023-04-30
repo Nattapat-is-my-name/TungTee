229a63e57d453b09df64e269dc06b77d1b5ee3ed
sora-k46 <sorrawit456789@gmail.com>
2023-04-30 17:35:41 +0700

    Create StreamBuilder for controlling authentication

---
+++ lib/Pages/auth_control.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tungtee/Pages/home.dart';
+import 'package:tungtee/Pages/login.dart';
+
+class AuthPageController extends StatelessWidget {
+  const AuthPageController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          }
+          return const LoginPage();
+        },
+        stream: FirebaseAuth.instance.authStateChanges(),
+      ),
+    );
+  }
+}
