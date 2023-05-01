5fb21352b76fbe8aa56c6ce870bac5bfc91d983c
Nattapat-is-my-name <bossnattapat238@gmail.com>
2023-05-02 01:07:59 +0700

    Update NavBar

---
+++ lib/Widgets/navbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      height: 80.0,
+      child: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              tooltip: 'Open popup menu',
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                final SnackBar snackBar = SnackBar(
+                  content: const Text('Yay! A SnackBar!'),
+                  action: SnackBarAction(
+                    label: 'Undo',
+                    onPressed: () {},
+                  ),
+                );
+
+                // Find the ScaffoldMessenger in the widget tree
+                // and use it to show a SnackBar.
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+            ),
+            IconButton(
+              tooltip: 'Search',
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: 'Favorite',
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
