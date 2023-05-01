8b96f6debefe653e6611f74a839c7fcba680f7b9
sora-k46 <sorrawit456789@gmail.com>
2023-05-02 00:32:12 +0700

    Create user_provider.dart

---
+++ lib/services/user_provider.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tungtee/models/event_model.dart';
+import 'package:tungtee/models/user_model.dart';
+
+class UserProvider {
+  final _userCollection = FirebaseFirestore.instance.collection('Users');
+  final _eventCollection = FirebaseFirestore.instance.collection('Events');
+
+  Future<UserModel> createUser(User user) async {
+    final UserModel newUser = UserModel(
+      userId: user.uid,
+      fullname: user.displayName!,
+      nickname: '',
+      email: user.email!,
+      phone: user.phoneNumber!,
+      gender: '',
+      birthDate: DateTime.now(),
+      interests: [],
+      createdEvents: [],
+      joinedEvents: [],
+      behaviorPoint: 3,
+    );
+    final DocumentReference docRef =
+        await _userCollection.add(newUser.toJSON());
+    final DocumentSnapshot docSnap = await docRef.get();
+    return UserModel.fromJSON(docSnap.data() as Map<String, dynamic>);
+  }
+
+  Future<UserModel> getUserById(String userId) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    final DocumentSnapshot docSnap = await docRef.get();
+    return UserModel.fromJSON(docSnap.data() as Map<String, dynamic>);
+  }
+}
