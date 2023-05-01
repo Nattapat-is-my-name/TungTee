b05bb4fb39e02316240f4f4f7c87dd6661483da8
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-05-01 23:32:41 +0700

    add button

--- lib/Pages/persona.dart
+++ lib/Pages/persona.dart
@@ -33,108 +33,137 @@ class Myform extends StatefulWidget {
 }
 
 class _MyformState extends State<Myform> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        Container(
-            margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color.fromRGBO(103, 80, 164, 0.1),
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+          children: [
+            Container(
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: const Color.fromRGBO(103, 80, 164, 0.1),
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🏈 Soccer"),
-                    Interest(title: "🏀 Basketball"),
-                    Interest(title: "⚽️ Football")
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🏈 Soccer"),
+                        Interest(title: "🏀 Basketball"),
+                        Interest(title: "⚽️ Football")
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🏋🏻 Body weight"),
+                        Interest(title: "🏐 VolleyBall"),
+                        Interest(title: "🎾 Tennis")
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🎨 Painting"),
+                        Interest(title: "✏️ Drawing"),
+                        Interest(title: "✍🏻 Writing")
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🎤 Pop"),
+                        Interest(title: "🎸 Rock"),
+                        Interest(title: "👨🏻‍🎤 Hip-hop"),
+                        Interest(title: "🎵 Jazz")
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🏖️ Beaches"),
+                        Interest(title: "⛰️ Moutains"),
+                        Interest(title: "🌆 City sightseeing"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🌍 International destination"),
+                        Interest(title: "🛣️ Road trips"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🍿 Movies"),
+                        Interest(title: "📺 TV shows"),
+                        Interest(title: "🎮 Video games"),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: const [
+                        Interest(title: "🎭 Stand-up comedy"),
+                        Interest(title: "🎯 Board games"),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            // If the form is valid, display a snackbar. In the real world,
+                            // you'd often call a server or save the information in a database.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.purple,
+                            fixedSize: const Size(1000, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50))),
+                        child: const Text('Submit'),
+                      ),
+                    ),
                   ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🏋🏻 Body weight"),
-                    Interest(title: "🏐 VolleyBall"),
-                    Interest(title: "🎾 Tennis")
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🎨 Painting"),
-                    Interest(title: "✏️ Drawing"),
-                    Interest(title: "✍🏻 Writing")
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🎤 Pop"),
-                    Interest(title: "🎸 Rock"),
-                    Interest(title: "👨🏻‍🎤 Hip-hop"),
-                    Interest(title: "🎵 Jazz")
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🏖️ Beaches"),
-                    Interest(title: "⛰️ Moutains"),
-                    Interest(title: "🌆 City sightseeing"),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🌍 International destination"),
-                    Interest(title: "🛣️ Road trips"),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🍿 Movies"),
-                    Interest(title: "📺 TV shows"),
-                    Interest(title: "🎮 Video games"),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: const [
-                    Interest(title: "🎭 Stand-up comedy"),
-                    Interest(title: "🎯 Board games"),
-                  ],
-                ),
-              ],
-            ))
-      ],
-    ));
+                ))
+          ],
+        ));
   }
 }
 
