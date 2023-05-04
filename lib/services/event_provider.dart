a5665855c82d7742da15eaa0e06a255427c48d65
sora-k46 <sorrawit456789@gmail.com>
2023-05-04 23:04:51 +0700

    Add descriptive comments to each function

--- lib/services/event_provider.dart
+++ lib/services/event_provider.dart
@@ -5,12 +5,16 @@ class EventProvider {
   final _eventCollection = FirebaseFirestore.instance
       .collection("Events"); // use _ to hide value from outside
 
+  /// Take `EventModel` as argument to add to Firestore database
+  ///
+  /// return `EventModel` that successfully added to Firestore database
   Future<EventModel> createEvent(EventModel event) async {
     final DocumentReference docRef = await _eventCollection.add(event.toJSON());
     final DocumentSnapshot docSnap = await docRef.get();
     return EventModel.fromJSON(docSnap.data() as Map<String, dynamic>);
   }
 
+  /// return `List<EventModel>` from Firestore database
   Future<List<EventModel>> getEvents() async {
     final QuerySnapshot querySnapshot = await _eventCollection.get();
     final List<EventModel> events = querySnapshot.docs
@@ -20,6 +24,9 @@ class EventProvider {
     return events;
   }
 
+  /// Get list of `EventModel` that its `tags` field contain any of `interests` (List<String>) argument
+  ///
+  /// return `List<EventModel>` from Firestore database
   Future<List<EventModel>> getEventsByInterests(List<String> interests) async {
     final QuerySnapshot querySnapshot =
         await _eventCollection.where('tags', arrayContainsAny: interests).get();
@@ -30,6 +37,7 @@ class EventProvider {
     return events;
   }
 
+  /// return `EventModel` that match its `eventId` field with `eventId` (String) argument
   Future<EventModel> getEventById(String eventId) async {
     final QuerySnapshot querySnapshot =
         await _eventCollection.where('eventId', isEqualTo: eventId).get();
@@ -37,6 +45,7 @@ class EventProvider {
         querySnapshot.docs.first.data() as Map<String, dynamic>);
   }
 
+  /// delete event by `eventId` (String)
   Future<void> deleteEventById(String eventId) async {
     _eventCollection.doc(eventId).delete();
   }
