579059df4575d74b0f05ff71e3a0eb63cecaf081
sora-k46 <sorrawit456789@gmail.com>
2023-05-06 00:05:31 +0700

    Handle unregistered sign-in

--- lib/Pages/auth_control.dart
+++ lib/Pages/auth_control.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tungtee/Pages/home.dart';
 import 'package:tungtee/Pages/login.dart';
+import 'package:tungtee/Pages/register.dart';
+import 'package:tungtee/Services/user_provider.dart';
 
 class AuthPageController extends StatelessWidget {
   const AuthPageController({super.key});
@@ -12,9 +14,23 @@ class AuthPageController extends StatelessWidget {
       body: StreamBuilder<User?>(
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomePage();
+            return FutureBuilder<bool>(
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  if (snapshot.data!) {
+                    return const HomePage();
+                  } else {
+                    return const RegisterPage();
+                  }
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+              future: UserProvider().isUserRegistered(snapshot.data!.uid),
+            );
+          } else {
+            return const LoginPage();
           }
-          return const LoginPage();
         },
         stream: FirebaseAuth.instance.authStateChanges(),
       ),
