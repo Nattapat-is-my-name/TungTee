7e649001575ebb26a9ffc9c4f7a98926caef191e
sora-k46 <sorrawit456789@gmail.com>
2023-05-05 20:17:38 +0700

    Create update operation for user service

--- lib/services/user_provider.dart
+++ lib/services/user_provider.dart
@@ -36,5 +36,97 @@ class UserProvider {
     return createdEvents;
   }
 
+  /* Updatable field
+   * fullname
+   * nickname
+   * email
+   * phone
+   * gender
+   * birth_date
+   * interests
+   * created_events
+   * joined_events
+   * behavior_point
+   * 
+   * Q: Why updating each field by separate function?
+   * A: It is easier to manage.
+   */
+
+  Future<void> updateUserFullName(String userId, String fullname) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'fullname': fullname});
+  }
+
+  Future<void> updateUserNickName(String userId, String nickname) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'nickname': nickname});
+  }
+
+  Future<void> updateUserEmail(String userId, String email) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'email': email});
+  }
+
+  Future<void> updateUserPhone(String userId, String phone) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'phone': phone});
+  }
+
+  Future<void> updateUserGender(String userId, String gender) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'gender': gender});
+  }
+
+  Future<void> updateUserBirthDate(String userId, DateTime birthDate) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'birthDate': birthDate.toIso8601String()});
+  }
+
+  Future<void> addInterest(String userId, String interest) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({
+      'interests': FieldValue.arrayUnion([interest])
+    });
+  }
+
+  Future<void> removeInterest(String userId, String interest) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({
+      'interests': FieldValue.arrayRemove([interest])
+    });
+  }
+
+  Future<void> joinEvent(String eventId, String userId) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({
+      'joinedEvents': FieldValue.arrayUnion([eventId])
+    });
+  }
+
+  Future<void> leftEvent(String eventId, String userId) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({
+      'joinedEvents': FieldValue.arrayRemove([eventId])
+    });
+  }
+
+  Future<void> reduceBehaviorPoint(String userId) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'behaviorPoint': FieldValue.increment(-1)});
+  }
+
+  Future<void> increaseBehaviorPoint(String userId) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'behaviorPoint': FieldValue.increment(1)});
+  }
+
+  Future<bool> isOnPenalty(String userId) async {
+    final UserModel user = await getUserById(userId);
+    return user.behaviorPoint <= 0;
+  }
+
+  Future<void> updateUserProfileImage(String userId, String image) async {
+    final DocumentReference docRef = _userCollection.doc(userId);
+    await docRef.update({'profileImage': image});
   }
 }
