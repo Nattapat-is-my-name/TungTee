7e4992fd60af5672c463297fbb21d2f2d271915f
Nattapat-is-my-name <bossnattapat238@gmail.com>
2023-05-08 03:38:28 +0700

    Create Event Form

--- lib/Pages/create_event.dart
+++ lib/Pages/create_event.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
 import 'package:intl/intl.dart';
-import 'package:tungtee/Pages/Register.dart';
+
+import '../widgets/dynamicchip.dart';
+import '../widgets/pick.dart';
 
 class Createevent extends StatefulWidget {
   const Createevent({super.key});
@@ -20,16 +21,21 @@ class _CreateeventState extends State<Createevent> {
     super.initState();
   }
 
+  double _startValue = 0.0;
+  double _endValue = 5.0;
+
   bool showWidget = false;
+  bool showWidget1 = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create Event'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Container(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
           child: Column(
             children: [
               TextFormField(
@@ -39,164 +45,221 @@ class _CreateeventState extends State<Createevent> {
                   labelText: 'Title',
                 ),
               ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                maxLength: 200,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Descscript Here',
-                  labelText: 'Detail',
-                ),
+              const SizedBox(
+                height: 15,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    width: 150,
-                    child: TextField(
-                      controller:
-                          dateController, //editing controller of this TextField
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
+                  Expanded(
+                    child: SizedBox(
+                      child: TextField(
+                        controller:
+                            dateController, //editing controller of this TextField
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
 
-                          //icon of text field
-                          labelText: "Enter Date" //label text of field
-                          ),
-                      readOnly: true, // when true user cannot edit text
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(), //get today's date
-                            firstDate: DateTime(
-                                2000), //DateTime.now() - not to allow to choose before today.
-                            lastDate: DateTime(2101));
+                            //icon of text field
+                            hintText: "Enter Date" //label text of field
+                            ),
+                        readOnly: true, // when true user cannot edit text
+                        onTap: () async {
+                          DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(), //get today's date
+                              firstDate: DateTime(
+                                  2000), //DateTime.now() - not to allow to choose before today.
+                              lastDate: DateTime(2101));
 
-                        if (pickedDate != null) {
-                          String formattedDate =
-                              DateFormat('yyyy-MM-dd').format(pickedDate);
+                          if (pickedDate != null) {
+                            String formattedDate =
+                                DateFormat('yyyy-MM-dd').format(pickedDate);
 
-                          setState(() {
-                            dateController.text = formattedDate;
-                          });
-                        } else {
-                          print("Date is not selected");
-                        }
-                      },
+                            setState(() {
+                              dateController.text = formattedDate;
+                            });
+                          } else {
+                            print("Date is not selected");
+                          }
+                        },
+                      ),
                     ),
                   ),
-                  Spacer(),
                   SizedBox(
-                    width: 150,
-                    child: TextField(
-                      controller:
-                          dateController, //editing controller of this TextField
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      child: TextField(
+                        controller:
+                            dateController, //editing controller of this TextField
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
 
-                          //icon of text field
-                          labelText: "Enter Date" //label text of field
-                          ),
-                      readOnly: true, // when true user cannot edit text
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(), //get today's date
-                            firstDate: DateTime(
-                                2000), //DateTime.now() - not to allow to choose before today.
-                            lastDate: DateTime(2101));
+                            //icon of text field
+                            hintText: "Enter Date" //label text of field
+                            ),
+                        readOnly: true, // when true user cannot edit text
+                        onTap: () async {
+                          DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(), //get today's date
+                              firstDate: DateTime(
+                                  2000), //DateTime.now() - not to allow to choose before today.
+                              lastDate: DateTime(2101));
 
-                        if (pickedDate != null) {
-                          String formattedDate =
-                              DateFormat('yyyy-MM-dd').format(pickedDate);
+                          if (pickedDate != null) {
+                            String formattedDate =
+                                DateFormat('yyyy-MM-dd').format(pickedDate);
 
-                          setState(() {
-                            dateController.text = formattedDate;
-                          });
-                        } else {
-                          print("Date is not selected");
-                        }
-                      },
+                            setState(() {
+                              dateController.text = formattedDate;
+                            });
+                          } else {
+                            print("Date is not selected");
+                          }
+                        },
+                      ),
                     ),
                   ),
                 ],
               ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    showWidget = !showWidget;
-                  });
-                },
-                child: const Icon(
-                  Icons.close,
-                  color: Color.fromARGB(255, 255, 0, 0),
-                ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        showWidget = !showWidget;
+                      });
+                    },
+                    label: Text("Set Age"),
+                    icon: Icon(Icons.add, size: 20),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        showWidget1 = !showWidget1;
+                      });
+                    },
+                    label: Text("Set Person"),
+                    icon: Icon(Icons.add, size: 20),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
               ),
               Column(
                 children: [
                   showWidget
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextButton(
-                              onPressed: () {},
-                              child: const Icon(Icons.ac_unit),
+                      ? Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Minimum Age',
+                              labelText: 'Set Age',
                             ),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Icon(Icons.accessible),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Icon(Icons.backpack),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Icon(Icons.cached),
-                            ),
-                          ],
+                          ),
                         )
                       : Container(),
                 ],
               ),
-              SizedBox(
-                width: 150,
-                child: TextField(
-                  controller:
-                      dateController, //editing controller of this TextField
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-
-                      //icon of text field
-                      labelText: "Enter Date" //label text of field
+              Column(
+                children: [
+                  showWidget1
+                      ? RangeSlider(
+                          min: 0,
+                          max: 20,
+                          divisions: 20,
+                          labels: RangeLabels(
+                            _startValue.round().toString(),
+                            _endValue.round().toString(),
+                          ),
+                          values: RangeValues(_startValue, _endValue),
+                          onChanged: (values) {
+                            setState(() {
+                              _startValue = values.start;
+                              _endValue = values.end;
+                            });
+                          },
+                        )
+                      : Container(),
+                ],
+              ),
+              const TextField(
+                maxLength: 200,
+                keyboardType: TextInputType.multiline,
+                maxLines: 4,
+                decoration: InputDecoration(
+                    hintText: "Enter Detail", border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Event Name',
+                    labelText: 'Location',
+                    prefixIcon: Icon(Icons.location_pin)),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Choice Hobbys"),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [dynamicChip()],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Choice Photo"),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Theme.of(context).colorScheme.outline,
                       ),
-                  readOnly: true, // when true user cannot edit text
-                  onTap: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(), //get today's date
-                        firstDate: DateTime(
-                            2000), //DateTime.now() - not to allow to choose before today.
-                        lastDate: DateTime(2101));
-
-                    if (pickedDate != null) {
-                      String formattedDate =
-                          DateFormat('yyyy-MM-dd').format(pickedDate);
-
-                      setState(() {
-                        dateController.text = formattedDate;
-                      });
-                    } else {
-                      print("Date is not selected");
-                    }
-                  },
-                ),
+                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: const SizedBox(
+                        width: double.infinity,
+                        height: 200,
+                        child: Imgpicker()),
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
     );
-    ;
   }
 }
