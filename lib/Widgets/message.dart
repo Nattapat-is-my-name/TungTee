23b4827aed764a72f7322895215822ce37d247a9
sora-k46 <sorrawit456789@gmail.com>
2023-05-09 20:11:39 +0700

    Change Message to be 80% of device width

--- lib/Widgets/message.dart
+++ lib/Widgets/message.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:tungtee/Constants/colors.dart';
 
 class Message extends StatelessWidget {
-  const Message({super.key, this.isOwnMessage = false, required this.message});
+  const Message({this.isOwnMessage = false, required this.message});
 
   final bool isOwnMessage;
   final String message;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return FractionallySizedBox(
+      widthFactor: 0.8, // กำหนดความกว้างเป็น 80%
+      child: Container(
+        alignment: Alignment.centerRight,
         padding: const EdgeInsets.all(16),
         margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: primaryColor.shade900,
         ),
-        child: Text(message,
-            style: const TextStyle(fontSize: 16, color: Colors.white)));
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 16, color: Colors.white),
+        ),
+      ),
+    );
   }
 }
