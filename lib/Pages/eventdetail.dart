d56370845c640cff08d60afd8fdf075b8e58a685
MrT-jesus-1995 <livemlg1995@gmail.com>
2023-05-10 22:52:05 +0700

    Revert "eventdetail CRUD"
    This reverts commit 0cb372f64282f096fdd796a729cf706b4767aa98.

--- lib/Pages/eventdetail.dart
+++ lib/Pages/eventdetail.dart
@@ -1,13 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tungtee/Pages/chat_event.dart';
 import 'package:tungtee/constants/colors.dart';
-import 'package:tungtee/services/event_provider.dart';
-import 'package:tungtee/services/user_provider.dart';
 
 class EventDetail extends StatefulWidget {
-  const EventDetail({super.key, required this.eventId});
-  final String eventId;
+  const EventDetail({super.key});
 
   @override
   State<EventDetail> createState() => _EventDetail();
@@ -21,186 +16,135 @@ class _EventDetail extends State<EventDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: EventProvider().getEventById(widget.eventId),
-        builder: (context, snapshot) {
-          final event = snapshot.data;
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(event!.eventTitle),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('หมูกระทะ'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            const Image(
+              image: NetworkImage(
+                  'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+              fit: BoxFit.fill,
+              height: 200,
+              width: double.infinity,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Chip(
+                    label: const Text(
+                      'Food',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    side: BorderSide.none,
+                    backgroundColor: rawPrimaryColor,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    '25 Jul 18 at 12:00 am - 2:00 pm',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-              body: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  children: [
-                    const Image(
-                      image: NetworkImage(
-                          'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-                      fit: BoxFit.fill,
-                      height: 200,
-                      width: double.infinity,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              'หมูกะทะ',
+                              style: TextStyle(
+                                fontSize: 30,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.location_on),
+                              Text(
+                                'Susco ,Thanon Puttabucha Bang Mod',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                          child: Row(
+                            children: const [
+                              Expanded(
+                                  child: Text(
+                                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Chip(
-                            label: const Text(
-                              "food", // !need to pull from boss-sora
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                            side: BorderSide.none,
-                            backgroundColor: rawPrimaryColor,
-                          ),
-                          const SizedBox(width: 10),
-                          const Text(
-                            '25 Jul 18 at 12:00 am - 2:00 pm', // !need to pull from boss-sora
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: const [
+                            Icon(Icons.cake_outlined),
+                            Text('14-18'),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Text('2/5'),
+                            Text('person'),
+                          ],
+                        ),
+                      ],
                     ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      event.eventTitle,
-                                      style: const TextStyle(
-                                        fontSize: 30,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Container(
-                                  margin:
-                                      const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                                  child: Row(
-                                    children: const [
-                                      Icon(Icons.location_on),
-                                      Text(
-                                        'Susco ,Thanon Puttabucha Bang Mod', // !need to pull from boss-sora
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                          child: Text(event.eventDescription)),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Column(
-                                  children: [
-                                    const Icon(Icons.cake_outlined),
-                                    Text(
-                                        '${event.ageRestriction.minimumAge} - ${event.ageRestriction.maximumAge}'),
-                                  ],
-                                ),
-                                Column(
-                                  children: const [
-                                    Text('2/5'), // !need to pull from boss-sora
-                                    Text('person'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          const Divider(),
-                          event.joinedUsers.contains(
-                                  FirebaseAuth.instance.currentUser!.uid)
-                              ?
+                  ),
+                  const Divider(),
+                  const MyBottomAppBar()
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                              // joined
-                              Row(
-                                  children: [
-                                    FilledButton(
-                                        onPressed: () {},
-                                        child: Row(
-                                          children: const [
-                                            Icon(Icons.check),
-                                            SizedBox(width: 10),
-                                            Text('Joined')
-                                          ],
-                                        )),
-                                    const SizedBox(width: 5),
-                                    FilledButton(
-                                        onPressed: () {
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      ChatEvent(event: const <
-                                                          String, dynamic>{})));
-                                        },
-                                        child: Row(
-                                          children: const [
-                                            Icon(Icons.chat_bubble_outline),
-                                            SizedBox(width: 10),
-                                            Text('Chat')
-                                          ],
-                                        )),
-                                  ],
-                                  // not joined
-                                )
-                              : Row(
-                                  children: [
-                                    FilledButton(
-                                        onPressed: () async {
-                                          await UserProvider().joinEvent(
-                                              widget.eventId,
-                                              FirebaseAuth
-                                                  .instance.currentUser!.uid);
-                                          initState();
-                                        },
-                                        child: const Text("Join")),
-                                    const SizedBox(
-                                      width: 5,
-                                    ),
-                                    OutlinedButton(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: const Text("Cancle"))
-                                  ],
-                                )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
-        });
+class MyBottomAppBar extends StatelessWidget {
+  const MyBottomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        FilledButton(onPressed: () {}, child: const Text("Join")),
+        const SizedBox(
+          width: 5,
+        ),
+        OutlinedButton(onPressed: () {}, child: const Text("Cancle"))
+      ],
+    );
   }
 }
