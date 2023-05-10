b3c188938e1c50756ffe520371519c8a2e093579
sora-k46 <sorrawit456789@gmail.com>
2023-05-10 13:56:46 +0700

    Add GestureDetector

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tungtee/Pages/profile.dart';
+import 'package:tungtee/Widgets/dynamicchip.dart';
 
 import '../Widgets/cardevent.dart';
 import 'package:flutter/material.dart';
@@ -19,131 +20,136 @@ class _HomePagesState extends State<HomePages> {
     final user = FirebaseAuth.instance.currentUser!;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 25, 20, 10),
-                child: Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      //Welcome Name
-                      Column(
-                        children: const [
-                          Text('Welcome Boss',
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 25, 20, 10),
+                  child: Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        //Welcome Name
+                        Column(
+                          children: const [
+                            Text('Welcome Boss',
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.33,
+                                    color: Color(0xff3b383e))),
+                            Text(
+                              // tungtee9sD (57:18316)
+                              'TUNG TEE',
                               style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w400,
-                                  height: 1.33,
-                                  color: Color(0xff3b383e))),
-                          Text(
-                            // tungtee9sD (57:18316)
-                            'TUNG TEE',
-                            style: TextStyle(
-                              fontSize: 36,
-                              fontWeight: FontWeight.w400,
-                              height: 1.2222222222,
-                              color: Color(0xff000000),
+                                fontSize: 36,
+                                fontWeight: FontWeight.w400,
+                                height: 1.2222222222,
+                                color: Color(0xff000000),
+                              ),
                             ),
+                          ],
+                        ),
+                        const Spacer(),
+                        Row(
+                          children: [
+                            const Icon(Icons.notifications_outlined),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const Profile()));
+                              },
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(user.photoURL!),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    //Search Bar
+                    Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 0, vertical: 16),
+                        child: TextField(
+                            decoration: InputDecoration(
+                          contentPadding:
+                              const EdgeInsets.symmetric(vertical: 15),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50.0),
                           ),
-                        ],
+                          prefixIcon: const Icon(Icons.search),
+                          hintText: 'Search Event',
+                        ))),
+                  ]),
+                ),
+                //Chip
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: const [DynamicChip()],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: const [
+                                Text(
+                                  'Event',
+                                  style: TextStyle(
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.2222222222,
+                                    color: Color(0xff000000),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                      const Spacer(),
-                      Row(
-                        children: [
-                          const Icon(Icons.notifications_outlined),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const Profile()));
-                            },
-                            child: CircleAvatar(
-                              backgroundImage: NetworkImage(user.photoURL!),
-                            ),
-                          ),
-                        ],
+                      const CardLayout(
+                        thumbnail: Image(
+                          image: NetworkImage(
+                              'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+                          fit: BoxFit.cover,
+                          height: 100,
+                          width: 80,
+                        ),
+                        title: 'หมู',
+                        subtitle:
+                            'Flutter continues to improve and expand its horizons. '
+                            'This text should max out at two lines and clip',
+                        toptitle: 'Fri 17 Mar 08:09',
+                        amountPerson: '5',
+                        maxPerson: '10',
                       ),
                     ],
                   ),
-
-                  //Search Bar
-                  Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 0, vertical: 16),
-                      child: TextField(
-                          decoration: InputDecoration(
-                        contentPadding:
-                            const EdgeInsets.symmetric(vertical: 15),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        prefixIcon: const Icon(Icons.search),
-                        hintText: 'Search Event',
-                      ))),
-                ]),
-              ),
-              //Chip
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: const [dynamicChip()],
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: const [
-                              Text(
-                                'Event',
-                                style: TextStyle(
-                                  fontSize: 36,
-                                  fontWeight: FontWeight.w400,
-                                  height: 1.2222222222,
-                                  color: Color(0xff000000),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    const CardLayout(
-                      thumbnail: Image(
-                        image: NetworkImage(
-                            'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-                        fit: BoxFit.cover,
-                        height: 100,
-                        width: 80,
-                      ),
-                      title: 'หมู',
-                      subtitle:
-                          'Flutter continues to improve and expand its horizons. '
-                          'This text should max out at two lines and clip',
-                      toptitle: 'Fri 17 Mar 08:09',
-                      amountPerson: '5',
-                      maxPerson: '10',
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
