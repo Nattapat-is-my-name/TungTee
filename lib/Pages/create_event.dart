97cbcbe33a6cab9ae75ab46708f353ec964706f5
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-05-16 18:04:59 +0700

    handle time in ios

--- lib/Pages/create_event.dart
+++ lib/Pages/create_event.dart
@@ -235,8 +235,8 @@ class _CreateeventState extends State<Createevent> {
 
                               if (pickedTime != null) {
                                 setState(() {
-                                  DateTime parsedTime = DateFormat.jm().parse(
-                                      pickedTime.format(context).toString());
+                                  DateTime parsedTime = DateFormat("HH:mm").parse(
+                                      "${pickedTime.hour.toString()}:${pickedTime.minute.toString()}");
 
                                   String formattedTime =
                                       DateFormat('HH:mm').format(parsedTime);
@@ -277,8 +277,8 @@ class _CreateeventState extends State<Createevent> {
 
                               if (pickedTime != null) {
                                 setState(() {
-                                  DateTime parsedTime = DateFormat.jm().parse(
-                                      pickedTime.format(context).toString());
+                                  DateTime parsedTime = DateFormat("HH:mm").parse(
+                                      "${pickedTime.hour.toString()}:${pickedTime.minute.toString()}");
 
                                   String formattedTime =
                                       DateFormat('HH:mm').format(parsedTime);
